89045375447cbf1cbfe2a7915d236daaeb2d9e93
Tahir Dibirov II <tahirdibirovsecond@gmail.com>
2024-07-10 15:15:31 +0400

    #

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
+import {ReadlineParser, SerialPort} from "serialport";
 import EngineInterpreter from "./interpreter/EngineInterpreter";
 import HIDService from "./services/HIDService";
 
+
 const main = async () => {
+
+   const devices = await SerialPort.list();
+   console.log(devices);
+   const port  = new SerialPort({
+      baudRate: 57600,
+      path: '/dev/ttyUSB0'
+   });
+   const parser = port.pipe(new ReadlineParser());
+   (await HIDService.getDeviceByName("log")).on("data", (data)=>{
+      const interpretedData  = EngineInterpreter.engineInterpreter(data).toString()
+      console.log(interpretedData)
+      port.write(interpretedData)
+   })
+   
+ console.log( await HIDService.getDevices())
    
 }
+
+main();
