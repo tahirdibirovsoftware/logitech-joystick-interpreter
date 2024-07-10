c650c42566071210aa87f8b72ccea5d2c934e0b6
Tahir Dibirov II <tahirdibirovsecond@gmail.com>
2024-07-10 12:17:06 +0400

    HID Service has been refactored

--- src/HIDService.ts
+++ src/HIDService.ts
@@ -1,16 +1,37 @@
-import HID from "node-hid"
+import HID from "node-hid";
 
 class HIDService {
+    private static instance: HIDService;
 
-    getDevices = async () => {
-        return await HID.devicesAsync();
+    private constructor() {}
+
+    public static getInstance(): HIDService {
+        if (!HIDService.instance) {
+            HIDService.instance = new HIDService();
+        }
+        return HIDService.instance;
     }
 
-    getDeviceByName = async (deviceName: string) => {
-        const path = (await this.getDevices()).find(device => device.manufacturer.toLowerCase().includes(deviceName.toLowerCase())).path;
-        return await HID.HIDAsync.open(path)
+    public async getDevices(): Promise<HID.Device[]> {
+        return HID.devices();
     }
 
+    public async getDeviceByName(deviceName: string): Promise<HID.HID> {
+        const devices = await this.getDevices();
+        const device = devices.find(dev => 
+            dev.manufacturer?.toLowerCase().includes(deviceName.toLowerCase())
+        );
+
+        if (!device) {
+            throw new Error(`Device with name '${deviceName}' not found`);
+        }
+
+        if (!device.path) {
+            throw new Error(`Device found but path is undefined`);
+        }
+
+        return new HID.HID(device.path);
+    }
 }
 
-export default new HIDService();
+export default HIDService.getInstance();
